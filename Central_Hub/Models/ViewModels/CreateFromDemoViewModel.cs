187355e3b71c925ddc31c8ad739e607a5973191f
jabulani Conjugation Sibeko <conjugationjabulani@gmail.com>
2026-01-02 12:49:28 +0200

    commit all

---
+++ Central_Hub/Models/ViewModels/CreateFromDemoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Central_Hub.Models.ViewModels
+{
+    public class CreateFromDemoViewModel
+    {
+        public ClientCompany Company { get; set; }
+        public CompanyAdministrator Administrator { get; set; }
+        public int? DemoRequestId { get; set; }
+    }
+}
